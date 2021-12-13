958eaadfa8b789a60decb224f27ab103c67238e3
jdw <johndward01@gmail.com>
2021-12-13 09:59:36 -0600

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myList = new List<int>();
+
+            Populater(myList);
+
+            Console.WriteLine($"Number of items in List: {myList.Count}");
+        }
+
+        private static void Populater(List<int> numberList)
+        {
+            for (int i = 0; i < 50; ++i)
+            {
+                Random rng = new Random();
+                var nums = rng.Next(0, 50);
+                numberList.Add(nums);
+            }
+        }
+
+    }
+}
